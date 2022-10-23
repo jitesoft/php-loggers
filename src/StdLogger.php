35675cd988e89fe13e41cd201742e6a9606fc06b
Johannes Tegnér <johannes@jitesoft.com>
2022-10-23 21:36:51 +0200

    fix: Stream logger is now abstract.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- src/StdLogger.php
+++ src/StdLogger.php
@@ -7,14 +7,24 @@ declare(strict_types=1);
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 namespace Jitesoft\Log;
 
+use Carbon\Carbon;
+use Jitesoft\Log\Traits\LogLevelTrait;
+use Jitesoft\Log\Traits\TextFormatterTrait;
+use Psr\Log\LogLevel;
+
 /**
  * A logger which outputs messages to stdout and stderr.
  *
  * @since 1.0.0
  */
 class StdLogger extends StreamLogger {
+    use TextFormatterTrait, LogLevelTrait;
+
     public const DEFAULT_FORMAT      = '[%s] %s: %s' . PHP_EOL;
     public const DEFAULT_TIME_FORMAT = 'H:i:s.v';
+
+    protected string $format;
+    protected string $timeFormat;
 
     /**
      * StdLogger constructor.
@@ -25,7 +35,43 @@ class StdLogger extends StreamLogger {
     public function __construct(string $format     = self::DEFAULT_FORMAT,
                                 string $timeFormat = self::DEFAULT_TIME_FORMAT
     ) {
-        parent::__construct($format, $timeFormat, null, null);
+        parent::__construct(null, null);
+
+        $this->format     = $format;
+        $this->timeFormat = $timeFormat;
+    }
+
+    protected function innerLog(string $level,
+        string $message,
+        array $context = []): void {
+        if (in_array(
+            $level,
+            [
+                LogLevel::EMERGENCY,
+                LogLevel::CRITICAL,
+                LogLevel::ALERT,
+                LogLevel::ERROR
+            ],
+            true
+        )
+        ) {
+            $stream = &$this->stderr;
+        } else {
+            $stream = &$this->stdout;
+        }
+
+        fwrite(
+            $stream,
+            $this->format(
+                sprintf(
+                    $this->format,
+                    Carbon::now()->format($this->timeFormat),
+                    strtoupper($level),
+                    $message
+                ),
+                $context
+            )
+        );
     }
 
 }
--- src/StreamLogger.php
+++ src/StreamLogger.php
@@ -1,20 +1,9 @@
 <?php
 namespace Jitesoft\Log;
 
-use Carbon\Carbon;
-use Jitesoft\Log\Traits\LogLevelTrait;
-use Jitesoft\Log\Traits\TextFormatterTrait;
 use Psr\Log\AbstractLogger;
-use Psr\Log\LogLevel;
 
-class StreamLogger extends AbstractLogger {
-    use TextFormatterTrait, LogLevelTrait;
-
-    public const DEFAULT_FORMAT      = '[%s] %s: %s' . PHP_EOL;
-    public const DEFAULT_TIME_FORMAT = 'H:i:s.v';
-
-    protected string $format;
-    protected string $timeFormat;
+abstract class StreamLogger extends AbstractLogger {
     protected $stdout;
     protected $stderr;
 
@@ -24,9 +13,7 @@ class StreamLogger extends AbstractLogger {
      * @param $errorStream ?resource Stream to log errors to. Defaults to stderr.
      * @param $outStream ?resource   Stream to log to. Defaults to stdout.
      */
-    public function __construct(string $format = self::DEFAULT_FORMAT,
-                                string $timeFormat = self::DEFAULT_TIME_FORMAT,
-                                $errorStream = null,
+    public function __construct($errorStream = null,
                                 $outStream = null) {
         $this->stderr = $errorStream ?? (defined('STDERR') ? STDERR : fopen(
             'php://stderr',
@@ -36,42 +23,6 @@ class StreamLogger extends AbstractLogger {
             'php://stdout',
             'wb'
         ));
-
-        $this->format     = $format;
-        $this->timeFormat = $timeFormat;
-    }
-
-    protected function innerLog(string $level,
-                                string $message,
-                                array $context = []): void {
-        if (in_array(
-            $level,
-            [
-                LogLevel::EMERGENCY,
-                LogLevel::CRITICAL,
-                LogLevel::ALERT,
-                LogLevel::ERROR
-            ],
-            true
-        )
-        ) {
-            $stream = &$this->stderr;
-        } else {
-            $stream = &$this->stdout;
-        }
-
-        fwrite(
-            $stream,
-            $this->format(
-                sprintf(
-                    $this->format,
-                    Carbon::now()->format($this->timeFormat),
-                    strtoupper($level),
-                    $message
-                ),
-                $context
-            )
-        );
     }
 
 }
